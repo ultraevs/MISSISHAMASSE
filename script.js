ce8170f481d05cf926f5f98c7b08a731d2f5505c
Crowdy <crowwwdy@gmail.com>
2023-11-10 21:21:47 +0300

    3st fornt commit

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ window.addEventListener('scroll', () => {
     }
     lastScroll = scrollPosition();
 })
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
