73a3017159a97c3ca5324527205c20de91a49fa4
Alexander Marchenko <alex@Air-Alexander.IGD_Rostelecom>
2023-11-10 20:27:54 +0300

    PostgreSQL added

--- python-backend/database.py
+++ python-backend/database.py
@@ -3,7 +3,7 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://cc85662_testdb:ueTprrn2@5.23.50.27:3306/cc85662_testdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://test:test@postgres/test_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 
 
