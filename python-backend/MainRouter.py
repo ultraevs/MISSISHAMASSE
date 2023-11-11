f4a7d4b4856d04d2528c3ac4aced8cc386648bfd
Alexander Marchenko <alex@MacBook-Air-Alexander.local>
2023-11-11 10:57:33 +0300

    Not perfect but yes

--- python-backend/MainRouter.py
+++ python-backend/MainRouter.py
@@ -10,3 +10,15 @@ async def get_main():
     with open('static/index.html', 'r') as html:
         data = html.read()
         return HTMLResponse(content=data)
+    
+@router.get('/tickets/')
+async def get_main():
+    with open('static/tickets.html', 'r') as html:
+        data = html.read()
+        return HTMLResponse(content=data)
+    
+@router.get('/routes/')
+async def get_main():
+    with open('static/route.html', 'r') as html:
+        data = html.read()
+        return HTMLResponse(content=data)
